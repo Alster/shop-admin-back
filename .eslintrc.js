57c4382d4d15f402cfc04c86707f9ec693e06c57
anikkhoma <anik123wb@gmail.com>
2024-10-27 11:01:31 +0200

    :hammer: updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: "latest",
 		project: "tsconfig.json",
-		// eslint-disable-next-line unicorn/prefer-module
-		tsconfigRootDir: __dirname,
+		tsconfigRootDir: "./",
 		sourceType: "module",
 		parser: "@typescript-eslint/parser",
 	},
